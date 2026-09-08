d2dfcd74065cffbc6c37eafccc321ad6828e4089
neeve-11 <55397645+neeve-11@users.noreply.github.com>
2026-09-09 00:36:10 +0530

    Main.java

---
+++ Main.java
@@ -0,0 +1,23 @@
+import Practice.Dto.Person;
+import Practice.List.ArrayListPractice;
+import Practice.Map.HashMapPractice;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+//        PriorityQueuePractice qp = new PriorityQueuePractice();
+//        qp.createMaxHeap();
+
+//        DequePractice dp = new DequePractice();
+//        dp.print(new Person());
+
+//        ArrayListPractice lp = new ArrayListPractice();
+//        lp.print(new Person());
+
+        HashMapPractice hmp = new HashMapPractice();
+        hmp.print(new Person());
+    }
+}
